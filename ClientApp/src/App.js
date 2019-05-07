1e5d2ece30442c1ae3bb62654000d4777937a4aa
Wei Wu <weiwu@Weis-MacBook-Pro.local>
2019-05-07 14:39:33 -0400

    add template for messages

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import Counter from './components/Counter';
+import Message from './components/Message';
 import FetchData from './components/FetchData';
 
 export default () => (
   <Layout>
     <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
+    <Route path='/message' component={Message} />
     <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
   </Layout>
 );
